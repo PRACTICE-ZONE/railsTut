8faed10551e119c38ea691d947b7a82318a6334a
IBUNHABIBU <ibunhabib@gmail.com>
2021-03-01 14:36:45 +0000

    commenting

--- app/controllers/files_controller.rb
+++ app/controllers/files_controller.rb
@@ -1,10 +1,10 @@
 class FilesController < ApplicationController
   def index 
-    if params[:next].present?
+    if params[:next]
       path = params[:next] + "/*"
       # @files = Dir[File.join(params[:next], "*")]
       @files = Dir[path]
-      @current = params[:next].split("/")[0..-2].join("/")
+      # @current = params[:next].split("/")[0..-2].join("/")
       @parent = path.split("/")[0..-3].join("/")
     else
       @files = Dir['*']
