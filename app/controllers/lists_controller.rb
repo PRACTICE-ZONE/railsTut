67557a54e6bb7db1b5e3ed7e0d9aac95bd6f3516
IBUNHABIBU <ibunhabib@gmail.com>
2021-03-03 19:30:30 +0000

    solving error

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,7 +4,7 @@ class ListsController < ApplicationController
     Dir["data/*.md"].each do |name|
       list = { }
       lines = File.read(name).split("\n")
-      list[:name] = line.shift
+      list[:name] = lines.shift
       list[:item] = []
       lines.each do |line| 
         list[:item] << { name: line[6..-1], done: line[3] == "x" }
