d96e58fad6e8ffb80b949fccdfb4035bfb0266db
IBUNHABIBU <ibunhabib@gmail.com>
2021-03-03 19:04:07 +0000

    adding lists

--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -2,7 +2,9 @@ class ListController < ApplicationController
   def index
     @lists = []
     Dir["data/*.md"].each do |name|
-      
+      list = { }
+      lines = File.read(name).split("\n")
+      list[:name] = line.shift
     end 
   end
 end
